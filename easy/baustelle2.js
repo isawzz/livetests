f6932aaa11e58ae6f65dd320ed704542e1ec6298
isawzz <iwgajam@gmail.com>
2022-07-02 07:31:07 -0700

    das

--- easy/baustelle2.js
+++ easy/baustelle2.js
@@ -1,12 +1,4 @@
 
-function output_loose_and_journeys(fen){
-
-	for(const j of fen.journeys){ console.log('journey', j.join(', ')); }
-	//console.log('journeys:', fen.journeys);
-
-	for (const plname in fen.players) { console.log('loosecards', plname, fen.players[plname].loosecards.join(', ')); }
-
-}
 
 
 
